70c66069c8d2b7c0e788d41c80bb07db1d6a06a8
Tara Furstenau <tfursten@asu.edu>
2020-05-15 16:10:01 -0700

    [skip travis] update build

--- recipe/conda_build.sh
+++ recipe/conda_build.sh
@@ -4,6 +4,9 @@ USER=tfursten
 OS=linux-64
 mkdir ~/conda-bld
 conda config --set anaconda_upload no
+conda config --add channels defaults
+conda config --add channels bioconda
+conda config --add channels conda-forge
 export CONDA_BLD_PATH=~/conda-bld
 export VERSION="1.0.0"
 conda build ..
